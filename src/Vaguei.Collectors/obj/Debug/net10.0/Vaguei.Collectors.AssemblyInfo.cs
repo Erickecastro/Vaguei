655f4ea7c24c2e7503fb06a5aa272e2f1b0eaceb
Ericke <eerickevh@gmail.com>
2026-08-31 12:20:38 -0400

    Melhora formatação das experiências

--- src/Vaguei.Collectors/obj/Debug/net10.0/Vaguei.Collectors.AssemblyInfo.cs
+++ src/Vaguei.Collectors/obj/Debug/net10.0/Vaguei.Collectors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vaguei.Collectors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba89395e485569abf25ca8ff5def95a1f75fe0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83557df0c8432d5afb623e5cedcdf73f165b1f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vaguei.Collectors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vaguei.Collectors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
